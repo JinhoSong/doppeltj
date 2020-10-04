bda0785dc3290edaadc34c3d538f5394309b6b83
JinhoSong <chosunz4@naver.com>
2020-10-04 19:42:03 +0900

    로그인 성공!

--- module-admin/src/main/java/com/predict/stock/config/SecurityConfig.java
+++ module-admin/src/main/java/com/predict/stock/config/SecurityConfig.java
@@ -1,38 +1,28 @@
 package com.predict.stock.config;
 
-import com.predict.stock.account.Account;
-import com.predict.stock.account.AccountRepository;
+
 import com.predict.stock.account.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    private final DataSource dataSource;
+    @Autowired
+    private  AccountService accountService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -51,20 +41,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .httpBasic();
     }
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        System.out.println("test");
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(passwordEncoder())
-                .usersByUsernameQuery("select username, password, enabled "
-                        + "from account "
-                        + "where username = ?")
-                .authoritiesByUsernameQuery("select a.username, r.name "
-                        + "from account_role ar inner join account a on ar.user_id = a.id "
-                        + "inner join role r on ar.role_id = a.id "
-                        + "where a.username = ?");
+
+    /* 인증방식 */
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(accountService).passwordEncoder(passwordEncoder);
     }
+
 
     @Override
     public void configure(WebSecurity web) throws Exception
